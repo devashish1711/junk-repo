3003f8b5f8bad408f78580c4bf8250ef3ed97b7c
devashish <devinfo1711@gmail.com>
2022-04-03 22:46:00 +0530

    error101 modified mama.js

--- mama.js
+++ mama.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-// this is second updated by creating new branch called master2
-=======
-// this is second editing made after creating new branch called master2
->>>>>>> master2
+// after creating error101 branch this is new edditing
